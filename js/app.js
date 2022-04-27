df43910bee6f7d79d6adfd0461151a836b044f54
vladian95 <kvankvan1995@gmail.com>
2022-04-27 22:12:11 +0300

    added styles

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 // 3D Scroll
+	
+
+
+
 
 let zSpacing = -1000,
 		lastPos = zSpacing / 5,
@@ -26,20 +30,20 @@ window.onscroll = function() {
 
 window.scrollTo(0, 1)
 
-// Audio
+// // Audio
 
-let soundButton = document.querySelector('.soundbutton'),
-		audio = document.querySelector('.audio')
+// let soundButton = document.querySelector('.soundbutton'),
+// 		audio = document.querySelector('.audio')
 
-soundButton.addEventListener('click', e => {
-	soundButton.classList.toggle('paused')
-	audio.paused ? audio.play() : audio.pause()
-})
+// soundButton.addEventListener('click', e => {
+// 	soundButton.classList.toggle('paused')
+// 	audio.paused ? audio.play() : audio.pause()
+// })
 
-window.onfocus = function() {
-	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
-}
+// window.onfocus = function() {
+// 	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+// }
 
-window.onblur = function() {
-	audio.pause()
-}
+// window.onblur = function() {
+// 	audio.pause()
+// }
